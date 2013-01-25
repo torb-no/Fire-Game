2669ade5c61b281b1a9180a4248c847a72b2905d
Torbjørn Vik Lunde <post@torbjornlunde.com>
2013-01-25 21:52:48 +0100

    First commit

---
+++ src/Material.java
@@ -0,0 +1,23 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: torbjorn
+ * Date: 25.01.13
+ * Time: 21:43
+ * To change this template use File | Settings | File Templates.
+ */
+
+import processing.core.*;
+
+public class Material {
+
+    PApplet p;
+
+    Material(PApplet parent) {
+        p = parent;
+    }
+
+    void iterate(Fire fire) {
+        // do stuff
+    }
+
+}
---
+++ src/main.java
@@ -0,0 +1,21 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: torbjorn
+ * Date: 25.01.13
+ * Time: 21:40
+ * To change this template use File | Settings | File Templates.
+ */
+
+import processing.core.*;
+
+public class main extends PApplet {
+
+    public void setup() {
+
+    }
+
+    public void draw() {
+
+    }
+
+}
