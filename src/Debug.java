affcb8416d01f588f4a0e1dd55a0d52067561f3e
Torbjørn Vik Lunde <post@torbjornlunde.com>
2013-02-04 18:40:39 +0100

    Improvements in debug class

--- src/Debug.java
+++ src/Debug.java
@@ -5,9 +5,21 @@ public class Debug {
     TApplet p;
     Level level;
 
+    PVector drawRect;
+    float drawRectSize;
+    int drawRectMode;
+    int drawRectColor;
+
+    PVector drawPoint;
+    int drawPointColor;
+
     Debug(TApplet parent, Level level) {
         p = parent;
         this.level = level;
+
+        drawRectColor = p.color(255, 0, 0, 150);
+        drawRectMode = p.CENTER;
+        drawPointColor = p.color(0, 255, 0);
     }
 
     public void iterate() {
@@ -15,10 +27,20 @@ public class Debug {
     }
 
     public void draw() {
-        // Fire hitbox
+        /* Fire hitbox
         p.fill(255, 120);
         p.rectMode(p.CENTER);
-        p.rect(level.fire.pos.x, level.fire.pos.y, level.fire.hitBox, level.fire.hitBox);
+        p.rect(level.fire.pos.x, level.fire.pos.y, level.fire.hitBox, level.fire.hitBox); // */
 
+        // If we want to draw something somewhere
+        if (drawRect != null) {
+            p.fill(drawRectColor);
+            p.rectMode(drawRectMode);
+            p.rect(drawRect.x, drawRect.y, drawRectSize, drawRectSize);
+        }
+        if (drawPoint != null) {
+            p.stroke(drawPointColor);
+            p.point(drawPoint.x, drawPoint.y);
+        }
     }
 }
