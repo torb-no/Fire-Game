addfa4f7358b49b8d5d3dc02bdf24c0ff2ddf90d
Torbjørn Vik Lunde <post@torbjornlunde.com>
2013-02-05 14:07:14 +0100

    Energy system for fire

--- src/Fire.java
+++ src/Fire.java
@@ -6,16 +6,26 @@ public class Fire {
     PVector pos;
     float hitBox = 10;
 
+    private final float energyMax = 30;
+    private float energy;
+
     Fire(TApplet parent, PVector position) {
         p = parent;
         this.pos = position;
+        energy = energyMax / 2;
     }
 
     public void draw() {
         p.noStroke();
         p.fill(200, 200, 0, 150);
-        p.ellipse(pos.x, pos.y, 20, 20);
+        p.ellipse(pos.x, pos.y, energy, energy);
     }
 
+    public void addEnergy(float amount) {
+        energy += amount;
+
+        if (energy < 0) energy = 0;
+        else if (energy > energyMax) energy = energyMax;
+    }
 
 }
