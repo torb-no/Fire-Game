97b23436c5f2cd9ee1ce0b6ae34a6b0a86e0a067
Torbjørn Vik Lunde <post@torbjornlunde.com>
2013-02-05 13:46:47 +0100

    Air material doesn’t make fire fall through solids

--- src/Material_Air.java
+++ src/Material_Air.java
@@ -1,12 +1,15 @@
 public class Material_Air extends Material {
 
+    public float downSpeed = 3;
+
     public boolean materialExistsWithinArea(float posX, float posY, float size) { return true; }
 
     public void draw() { /* air is invisible! */ }
 
     public boolean fireIteration() {
         // Fall downwards…
-        fire.pos.y += 3;
+        if (!level.positionIsSpecificMaterial(fire.pos.x, fire.pos.y+downSpeed, Material_Stable.class))
+            fire.pos.y += downSpeed;
         return false; //For consistency with the rest of the system, not really needed since air should always reside in the bottom stack
     }
 
